6892b596c6607ac6be9f20d62a099f8803ef9557
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-13 01:23:05 +0800

    Update and rename sk5_auto.shs to sk5_auto.sh

--- sk5_auto.sh
+++ sk5_auto.sh
@@ -1,4 +1,3 @@
-  
 #!/bin/bash
 sudo -i
 yum -y install wget && wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
