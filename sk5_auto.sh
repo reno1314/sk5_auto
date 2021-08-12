e608435aef8256f6ad204f0730ec21cf5338edf5
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-13 01:35:40 +0800

    Update sk5_auto.sh

--- sk5_auto.sh
+++ sk5_auto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo -i
 yum -y install wget && wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
+
+
