5353ea5a8540c841e8f0f54d447ff3d84c02155d
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-15 07:02:35 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo -i
 yum -y install wget && wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
