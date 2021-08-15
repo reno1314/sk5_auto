80e7fdbb893d7bac823702853f3513ada3a39a5e
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-15 11:45:29 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -2,8 +2,11 @@
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
 echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
+$ sudo !!
 echo "@reboot sleep 20 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
+$ sudo !!
 echo "@reboot sleep 30 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
+$ sudo !!
 
 
 
