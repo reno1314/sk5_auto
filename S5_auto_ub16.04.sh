826966e326bb859cff2c5638dc3c3ac3d6365b99
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-11-27 04:03:26 +0800

    Update S5_auto_ub16.04.sh

--- S5_auto_ub16.04.sh
+++ S5_auto_ub16.04.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 
 exit
