7cba75731cb42bca51f523f74cfb4e52f38749e9
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-12-16 12:22:16 +0800

    Update az_sk5_auto.sh

--- az_sk5_auto.sh
+++ az_sk5_auto.sh
@@ -2,17 +2,6 @@
 #!/bin/bash
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
-
-echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
-echo "@reboot sleep 20 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
-
 echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/azureuser
-echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/azure
-
-echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/centos
-echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/ubuntu
-echo "@reboot sleep 10 && bash /root/sk5_auto.sh" >> /var/spool/cron/debian
-
-
 
 exit
