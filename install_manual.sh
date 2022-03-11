87d76b5dd1a332ebb0b49f5ce4bb719f7d1cc7d1
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-03-11 21:05:21 +0800

    Update and rename S5_auto_ub16.04.sh to install_manual.sh

--- install_manual.sh
+++ install_manual.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
-bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
 echo "@reboot sleep 30 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 
