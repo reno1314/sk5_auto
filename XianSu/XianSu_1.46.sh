213c0a824d6b372c835f5108f0f4cf1565fc170d
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-05 10:01:28 +0800

    Update XianSu_1.46.sh

--- XianSu/XianSu_1.46.sh
+++ XianSu/XianSu_1.46.sh
@@ -6,6 +6,6 @@ sudo make install
 sudo systemctl enable wondershaper.service
 wondershaper -c -a eth0
 wondershaper -a eth0 -d 1500 -u 1500
-echo "@reboot sleep 40 && bash /root/XianSu_ZQ_1.46.sh" >> /var/spool/cron/root
+echo "@reboot sleep 50 && bash /root/XianSu_ZQ_1.46.sh" >> /var/spool/cron/root
 
 exit
