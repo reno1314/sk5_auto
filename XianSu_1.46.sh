0c8241fc8fd29cefd6f6f9195eef55ed814de7ba
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-05 10:21:33 +0800

    Update XianSu_1.46.sh

--- XianSu_1.46.sh
+++ XianSu_1.46.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/XianSu_ZQ_1.46.sh -O XianSu_ZQ_1.46.sh && chmod +x XianSu_ZQ_1.46.sh
 sudo yum -y install git
 git clone https://github.com/reno1314/wondershaper.git
 cd wondershaper
