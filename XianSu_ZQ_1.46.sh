e0a636f386c3e4da56581e5672865964104b027d
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-05 12:08:40 +0800

    Update XianSu_ZQ_1.46.sh

--- XianSu_ZQ_1.46.sh
+++ XianSu_ZQ_1.46.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd wondershaper
-sudo make install
 wondershaper -c -a eth0
 wondershaper -a eth0 -d 1500 -u 1500
-echo "@reboot sleep 40 && bash /root/XianSu_ZQ_1.46.sh" >> /var/spool/cron/root
 
 exit
