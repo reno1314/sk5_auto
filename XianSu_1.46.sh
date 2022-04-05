cc3f06a1a890991b7a49060b4096190062be8920
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-05 13:12:31 +0800

    Update XianSu_1.46.sh

--- XianSu_1.46.sh
+++ XianSu_1.46.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/XianSu_ZQ_1.46.sh -O XianSu_ZQ_1.46.sh && chmod +x XianSu_ZQ_1.46.sh
+mv /root/XianSu_ZQ_1.46.sh /etc/rc.d/init.d
+cd /etc/rc.d/init.d
+chkconfig --add XianSu_ZQ_1.46.sh
+chkconfig XianSu_ZQ_1.46.sh on
+cd /root
+
 sudo yum -y install git
 git clone https://github.com/reno1314/wondershaper.git
 cd wondershaper
@@ -7,6 +13,5 @@ sudo make install
 sudo systemctl enable wondershaper.service
 wondershaper -c -a eth0
 wondershaper -a eth0 -d 1500 -u 1500
-echo "@reboot sleep 35 && bash /root/XianSu_ZQ_1.46.sh" >> /var/spool/cron/root
 
 exit
