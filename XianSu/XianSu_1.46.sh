4fc8e81492d29c8cf13adc0ec05c4f60d0a2cf08
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-05 09:58:52 +0800

    Create XianSu_1.46.sh

---
+++ XianSu/XianSu_1.46.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum -y install git
+git clone https://github.com/reno1314/wondershaper.git
+cd wondershaper
+sudo make install
+sudo systemctl enable wondershaper.service
+wondershaper -c -a eth0
+wondershaper -a eth0 -d 1500 -u 1500
+echo "@reboot sleep 40 && bash /root/XianSu_ZQ_1.46.sh" >> /var/spool/cron/root
+
+exit
