e9ef5fbbfa9e5ec377c93964d5adc1b6f88e08b5
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-06 12:26:12 +0800

    Update XianSu_1.46_S5_auto.sh

--- XianSu_1.46_S5_auto.sh
+++ XianSu_1.46_S5_auto.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-sudo yum -y install wget
-wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
-wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/XianSu_ZQ_1.46.sh -O XianSu_ZQ_1.46.sh && chmod +x XianSu_ZQ_1.46.sh
+cd /root
+touch sk5_auto.sh
+chmod +x sk5_auto.sh
+cat >>sk5_auto.sh<<EOF
+#!/bin/bash
+for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
+/etc/init.d/sockd start
+exit
+EOF
+touch XianSu_ZQ_1.46.sh
+chmod +x XianSu_ZQ_1.46.sh
+cat >>XianSu_ZQ_1.46.sh<<EOF
+#!/bin/bash
+wondershaper -a eth0 -c
+wondershaper -a eth0 -d 1500 -u 1500
+exit
+EOF
 chmod +x /etc/rc.d/rc.local
 echo "/root/sk5_auto.sh" >> /etc/rc.d/rc.local
 echo "/root/XianSu_ZQ_1.46.sh" >> /etc/rc.d/rc.local
+cd /root
+sudo yum -y install wget
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 sudo yum -y install git
 git clone https://github.com/reno1314/wondershaper.git
