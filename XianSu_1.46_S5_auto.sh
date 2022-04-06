62723ccec2eb63a50e4c757c95016ac40a227ed8
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-06 23:59:30 +0800

    Update XianSu_1.46_S5_auto.sh

--- XianSu_1.46_S5_auto.sh
+++ XianSu_1.46_S5_auto.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 cd /root
-touch sk5_auto.sh
-chmod +x sk5_auto.sh
-cat >>sk5_auto.sh<<EOF
+touch sk5_auto_XS1.46.sh
+chmod +x sk5_auto_XS1.46.sh
+cat >>sk5_auto_XS1.46.sh<<EOF
 #!/bin/bash
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 /etc/init.d/sockd start
-exit
-EOF
-touch XianSu_ZQ_1.46.sh
-chmod +x XianSu_ZQ_1.46.sh
-cat >>XianSu_ZQ_1.46.sh<<EOF
-#!/bin/bash
 wondershaper -a eth0 -c
 wondershaper -a eth0 -d 1500 -u 1500
 exit
 EOF
 chmod +x /etc/rc.d/rc.local
-echo "/root/sk5_auto.sh" >> /etc/rc.d/rc.local
-echo "/root/XianSu_ZQ_1.46.sh" >> /etc/rc.d/rc.local
+echo "/root/sk5_auto_XS1.46.sh" >> /etc/rc.d/rc.local
 cd /root
+echo "@reboot sleep 45 && bash /root/sk5_auto_XS1.46.sh" >> /var/spool/cron/root
+
 sudo yum -y install wget
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 sudo yum -y install git
