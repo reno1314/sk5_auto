da19c5185f64e0e6e6914e8e7ad0310bf98bc476
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-07 15:01:43 +0800

    Update XianSu_1.46.sh

--- XianSu_1.46.sh
+++ XianSu_1.46.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/XianSu_ZQ_1.46.sh -O XianSu_ZQ_1.46.sh && chmod +x XianSu_ZQ_1.46.sh
-mv /root/XianSu_ZQ_1.46.sh /etc/rc.d/init.d
-cd /etc/rc.d/init.d
-chkconfig --add XianSu_ZQ_1.46.sh
-chkconfig XianSu_ZQ_1.46.sh on
 cd /root
+touch sk5_auto_XS1.46.sh
+chmod +x sk5_auto_XS1.46.sh
+cat >>sk5_auto_XS1.46.sh<<EOF
+#!/bin/bash
+for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
+/etc/init.d/sockd start
+wondershaper -a eth0 -c
+wondershaper -a eth0 -d 1500 -u 1500
+exit
+EOF
+chmod +x /etc/rc.d/rc.local
+echo "/root/sk5_auto_XS1.46.sh" >> /etc/rc.d/rc.local
+cd /root
+echo "@reboot sleep 45 && bash /root/sk5_auto_XS1.46.sh" >> /var/spool/cron/root
 
+sudo yum -y install wget
 sudo yum -y install git
 git clone https://github.com/reno1314/wondershaper.git
 cd wondershaper
 sudo make install
-wondershaper -c -a eth0
+wondershaper -a eth0 -c
 wondershaper -a eth0 -d 1500 -u 1500
 
 exit
