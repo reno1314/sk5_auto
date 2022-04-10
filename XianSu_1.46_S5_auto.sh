5c52465ea2b3696f0cbecc29c5a29a8cd540a7bd
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-11 03:15:04 +0800

    Update XianSu_1.46_S5_auto.sh

--- XianSu_1.46_S5_auto.sh
+++ XianSu_1.46_S5_auto.sh
@@ -3,19 +3,26 @@ rm -f /root/sk5_auto_XS1.46.sh
 cd /root
 touch /root/sk5_auto_XS1.46.sh
 chmod +x /root/sk5_auto_XS1.46.sh
-cat >>sk5_auto_XS1.46.sh<<EOF
+cat >>/root/sk5_auto_XS1.46.sh<<EOF
 #!/bin/bash
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 /etc/init.d/sockd start
 wondershaper -c -a eth0
-wondershaper -a eth0 -u 1540
+wondershaper -a eth0 -u 1600
 exit
 EOF
 cd /root
 
+rm -f /etc/rc.d/rc.local
+touch /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
-echo "/root/sk5_auto_XS1.46.sh" >> /etc/rc.d/rc.local
-
+cat >>/etc/rc.d/rc.local<<EOF
+#!/bin/bash
+touch /var/lock/subsys/local
+/root/sk5_auto_XS1.46.sh
+exit
+EOF
+cd /root
 rm -f /var/spool/cron/root
 touch /var/spool/cron/root
 
@@ -26,7 +33,7 @@ git clone https://github.com/reno1314/wondershaper.git
 cd wondershaper
 sudo make install
 wondershaper -c -a eth0
-wondershaper -a eth0 -u 1540
+wondershaper -a eth0 -u 1600
 
 rm -f /root/XianSu_1.46_S5_auto.sh
 
