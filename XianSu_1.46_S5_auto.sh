058b9fecc3f6a7034aaa29712225dcc789174e35
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-11 03:21:45 +0800

    Update XianSu_1.46_S5_auto.sh

--- XianSu_1.46_S5_auto.sh
+++ XianSu_1.46_S5_auto.sh
@@ -25,6 +25,7 @@ EOF
 cd /root
 rm -f /var/spool/cron/root
 touch /var/spool/cron/root
+echo "@reboot sleep 35 && bash /root/sk5_auto_XS1.46.sh" >> /var/spool/cron/root
 
 sudo yum -y install wget
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
