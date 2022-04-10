b45a28af5cade6ace89f545e399bf025f2e7d2d4
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-11 00:27:26 +0800

    Update XianSu_1.46.sh

--- XianSu_1.46.sh
+++ XianSu_1.46.sh
@@ -4,8 +4,8 @@ touch sk5_auto_XS1.46.sh
 chmod +x sk5_auto_XS1.46.sh
 cat >>sk5_auto_XS1.46.sh<<EOF
 #!/bin/bash
-wondershaper -a eth0 -c
-wondershaper -a eth0 -d 1500 -u 1500
+wondershaper -c -a eth0
+wondershaper -a eth0 -d 1540 -u 1540
 exit
 EOF
 chmod +x /etc/rc.d/rc.local
@@ -18,7 +18,7 @@ sudo yum -y install git
 git clone https://github.com/reno1314/wondershaper.git
 cd wondershaper
 sudo make install
-wondershaper -a eth0 -c
-wondershaper -a eth0 -d 1500 -u 1500
+wondershaper -c -a eth0
+wondershaper -a eth0 -d 1540 -u 1540
 
 exit
