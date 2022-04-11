dc7cf42e40f185e159b861efe8c2af0169964f25
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-04-11 13:22:41 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -7,8 +7,6 @@ cat >>/root/sk5_auto_XS1.46.sh<<EOF
 #!/bin/bash
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 /etc/init.d/sockd start
-wondershaper -c -a eth0
-wondershaper -a eth0 -u 1580
 exit
 EOF
 cd /root
