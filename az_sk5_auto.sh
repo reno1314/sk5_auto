a4e23ad042bfb2f3b6e59e796289148ec1447f9a
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-05-06 23:13:52 +0800

    Update az_sk5_auto.sh

--- az_sk5_auto.sh
+++ az_sk5_auto.sh
@@ -3,7 +3,8 @@ rm -f /root/install.sh
 rm -f /root/install_auto.sh
 rm -f /root/sk5_auto.sh
 rm -f /var/spool/cron/root
-sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
+for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
+wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
 /etc/init.d/sockd start
 wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
 echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
