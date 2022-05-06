dd5bad5f3ded5c431c4ad502a9adca10f0b021cf
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-05-06 23:05:00 +0800

    Update az_sk5_auto.sh

--- az_sk5_auto.sh
+++ az_sk5_auto.sh
@@ -2,37 +2,9 @@
 rm -f /root/install.sh
 rm -f /root/install_auto.sh
 rm -f /root/sk5_auto.sh
-rm -f /root/sk5_auto_XS1.46.sh
-cd /root
-touch /root/sk5_auto_XS1.46.sh
-chmod +x /root/sk5_auto_XS1.46.sh
-cat >>/root/sk5_auto_XS1.46.sh<<EOF
-#!/bin/bash
-for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
-/etc/init.d/sockd start
-exit
-EOF
-cd /root
-
-rm -f /etc/rc.d/rc.local
-touch /etc/rc.d/rc.local
-chmod +x /etc/rc.d/rc.local
-cat >>/etc/rc.d/rc.local<<EOF
-#!/bin/bash
-touch /var/lock/subsys/local
-/root/sk5_auto_XS1.46.sh
-exit
-EOF
-cd /root
 rm -f /var/spool/cron/root
-touch /var/spool/cron/root
-echo "@reboot sleep 35 && bash /root/sk5_auto_XS1.46.sh" >> /var/spool/cron/root
+sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
+/etc/init.d/sockd start
+wget --no-check-certificate https://raw.githubusercontent.com/reno1314/sk5_auto/master/sk5_auto.sh -O sk5_auto.sh && chmod +x sk5_auto.sh
+echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 
-for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
-
-sudo yum -y install wget
-wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
-
-rm -f /root/XianSu_1.46_S5_auto.sh
-
-exit
