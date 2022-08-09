4ae1c2280ef1713d50fc8a4143b70e2065ead29c
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-08-09 22:09:31 +0800

    Update az_sk5_auto.sh

--- az_sk5_auto.sh
+++ az_sk5_auto.sh
@@ -25,4 +25,7 @@ echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh
 
+rm -f /root/XianSu_1.46_S5_auto.sh
+rm -f /root/XianSu_1.52_S5_auto.sh
+
 exit
