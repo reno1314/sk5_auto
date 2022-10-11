f81fc5d51e8098faac333559e566d406323ace44
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-10-12 00:34:53 +0800

    Update openvxx.sh

--- openvxx.sh
+++ openvxx.sh
@@ -198,8 +198,8 @@ else
 	esac
 	echo ""
 	echo "选着需要的端口（默认12479）?"
-	read -p "Port: " -e -i 12479 PORT
-	read \n
+	# read -p "Port: " -e -i 12479 PORT
+	PORT=12479
 	echo ""
 	echo "选着需要的DNS（默认本机DNS)"
 	echo "   1) Current system resolvers"
