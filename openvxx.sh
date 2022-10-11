a0daf750acb2c20b8336de87095fe12967230627
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-10-11 23:44:28 +0800

    Update openvxx.sh

--- openvxx.sh
+++ openvxx.sh
@@ -183,7 +183,9 @@ else
 	echo "First I need to know the IPv4 address of the network interface you want OpenVPN"
 	echo "listening to."
 	read -p "IP address: " -e -i $IP IP
-	echo "\n"
+	echo ""
+	expect "IP address:"
+	send "\n"
 	echo "选着要用的传输方式，建议使用UDP速度更快"
 	echo "   1) UDP (recommended)"
 	echo "   2) TCP"
