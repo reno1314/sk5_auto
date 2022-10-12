c14eecfec6fb3ddedc40dd4ad4c8bdc03acff289
reno1314 <80469553+reno1314@users.noreply.github.com>
2022-10-12 16:13:50 +0800

    Update openvpx.sh

--- openvpx.sh
+++ openvpx.sh
@@ -200,7 +200,8 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	done
 	echo
 	echo "Enter a name for the first client:"
-	read -p "Name [client]: " unsanitized_client
+	# read -p "Name [client]: " unsanitized_client
+	unsanitized_client=client
 	# Allow a limited set of characters to avoid conflicts
 	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
 	[[ -z "$client" ]] && client="client"
