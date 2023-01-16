04d17a89dede548b42c3bb0c994bd3db1fe386dd
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-01-17 07:35:21 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -26,7 +26,7 @@ touch /var/spool/cron/root
 echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
-sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
+sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=yyf --passwd=123 && /etc/init.d/sockd start
 
 rm -f /root/XianSu_1.46_S5_auto.sh
 rm -f /root/XianSu_1.52_S5_auto.sh
