0336f2c38a173233c59cff3cc662b259d06de97c
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-02-13 18:11:18 +0800

    Update xnwk_30.sh

--- xnwk_30.sh
+++ xnwk_30.sh
@@ -25,4 +25,6 @@ rm -f /var/spool/cron/root
 touch /var/spool/cron/root
 echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 
+for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
+
 exit
