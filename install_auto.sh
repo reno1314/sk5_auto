e6b22e763477dc91c25d1f532f101d3ffa31ee24
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-03-29 12:11:10 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -f /root/xnwk_30.sh
 rm -f /root/sk5_auto.sh
+rm -f /root/install.sh
 rm -f /root/install_auto.sh
 rm -f /root/install_auto_tcp.sh
 rm -f /root/az_sk5_auto.sh
@@ -7,7 +9,7 @@ rm -f /root/sk5_auto_XS1.46.sh
 rm -f /root/sk5_auto_XS1.52.sh
 rm -f /root/XianSu_1.46_S5_auto.sh
 rm -f /root/XianSu_1.52_S5_auto.sh
-rm -f /root/install.sh
+sed -i '/@reboot sleep 35 \&\& bash \/root\/sk5_auto.sh/d' /var/spool/cron/root
 
 cd /root
 touch /root/sk5_auto.sh
