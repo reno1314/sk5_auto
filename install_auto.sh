85692ce285d25e572ab363c507af3905552b0012
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-03-29 14:19:35 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -122,4 +122,7 @@ fi
 
 sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 
+rm -f /root/XianSu_1.46_S5_auto.sh
+rm -f /root/XianSu_1.52_S5_auto.sh
+
 exit
