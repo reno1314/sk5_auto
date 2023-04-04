69f1559b038965e74c26fb9dc764e5d317d979c1
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-04-04 14:56:26 +0800

    Update sk5_auto.sh

--- sk5_auto.sh
+++ sk5_auto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
+for((i=5;i<=11;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 /etc/init.d/sockd start
 
 exit
