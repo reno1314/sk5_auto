54dfbada7d76a901acdcc1a904957507a82bad2d
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-05-04 13:47:56 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -102,9 +102,6 @@ fi
 
 sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
 
-rm -f /root/XianSu_1.46_S5_auto.sh
-rm -f /root/XianSu_1.52_S5_auto.sh
-
 # 判断主网卡的内网地址是否为 10.0.0.*
 if [[ $IP =~ ^10\.0\.0\..* ]]; then
 
