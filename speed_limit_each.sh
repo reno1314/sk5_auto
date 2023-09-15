5344c2e24e3a6b78e2e32a212464f7810adb3373
reno1314 <80469553+reno1314@users.noreply.github.com>
2023-09-15 23:30:28 +0800

    Update speed_limit_each.sh

--- speed_limit_each.sh
+++ speed_limit_each.sh
@@ -130,8 +130,10 @@ delete_traffic_control() {
   # 重新加载 systemd 管理的服务
   systemctl daemon-reload
 
-  rm -f /root/speed_limit_each.sh
-  
+  # 删除文件
+  #rm -f /root/speed_limit_each.sh
+  find / -name "speed_limit_each.sh" -type f -exec rm -f {} \;
+
   echo "已删除所有的限速规则及服务。"
 }
 
