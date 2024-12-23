928fe7675b8ffcd4ad24bf27dc1088626e5abe10
reno1314 <80469553+reno1314@users.noreply.github.com>
2024-12-23 19:14:19 +0800

    Update speed_new_7.sh

--- speed_new_7.sh
+++ speed_new_7.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -f /root/control.sh
 curl -kLs https://raw.githubusercontent.com/reno1314/sk5_auto/master/speed_limit_each_new_7.sh -o control.sh && chmod +x control.sh && ./control.sh set
 
