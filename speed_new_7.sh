1159dc3253d6393a29dcb47fa9190bdf392e07f0
reno1314 <80469553+reno1314@users.noreply.github.com>
2024-12-23 19:18:21 +0800

    Update speed_new_7.sh

--- speed_new_7.sh
+++ speed_new_7.sh
@@ -3,6 +3,5 @@ rm -f /root/control.sh
 curl -kLs https://raw.githubusercontent.com/reno1314/sk5_auto/master/speed_limit_each_new_7.sh -o control.sh && chmod +x control.sh && ./control.sh set
 
 cd /root
-rm -f /var/spool/cron/root
-touch /var/spool/cron/root
+[ ! -f /var/spool/cron/root ] && touch /var/spool/cron/root
 echo "@reboot sleep 40 && bash /root/control.sh set" >> /var/spool/cron/root
