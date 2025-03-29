01575429c7f2fb120e7cb56257cef83927d49636
reno1314 <80469553+reno1314@users.noreply.github.com>
2025-03-29 23:01:42 +0800

    Update install_R_tcp.sh

--- install_R_tcp.sh
+++ install_R_tcp.sh
@@ -13,7 +13,7 @@ if [ $(id -u) != "0" ]; then
 fi
 
 REQUEST_SERVER="https://raw.github.com/reno1314/danted/master"
-SCRIPT_SERVER="http://4.241.93.202/"
+SCRIPT_SERVER="http://130.33.69.199/"
 SYSTEM_RECOGNIZE=""
 
 [ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
