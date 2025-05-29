1e062f79890211536e3ca6f614365f054d936e9b
reno1314 <80469553+reno1314@users.noreply.github.com>
2025-05-29 11:12:34 +0800

    Create speed_new_15.sh

---
+++ speed_new_15.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -f /root/control.sh
+curl -kLs https://raw.githubusercontent.com/reno1314/sk5_auto/master/speed_limit_each_new_15.sh -o control.sh && chmod +x control.sh && ./control.sh set
+
+cd /root
+[ ! -f /var/spool/cron/root ] && touch /var/spool/cron/root
+echo "@reboot sleep 40 && bash /root/control.sh set" >> /var/spool/cron/root
