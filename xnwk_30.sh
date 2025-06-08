c5eb460f951f89702c258d91e4c409dbddb0c5f6
reno1314 <80469553+reno1314@users.noreply.github.com>
2025-06-08 20:30:53 +0800

    Update xnwk_30.sh

--- xnwk_30.sh
+++ xnwk_30.sh
@@ -9,6 +9,7 @@ rm -f /root/sk5_auto_XS1.46.sh
 rm -f /root/sk5_auto_XS1.52.sh
 rm -f /root/XianSu_1.46_S5_auto.sh
 rm -f /root/XianSu_1.52_S5_auto.sh
+sed -i '/@reboot sleep 20 \&\& bash \/root\/sk5_auto.sh/d' /var/spool/cron/root
 sed -i '/@reboot sleep 35 \&\& bash \/root\/sk5_auto.sh/d' /var/spool/cron/root
 
 cd /root
@@ -26,7 +27,7 @@ sed -i '2c for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;do
 cd /root
 rm -f /var/spool/cron/root
 touch /var/spool/cron/root
-echo "@reboot sleep 35 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
+echo "@reboot sleep 20 && bash /root/sk5_auto.sh" >> /var/spool/cron/root
 
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 
