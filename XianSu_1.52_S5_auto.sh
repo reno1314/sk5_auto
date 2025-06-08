326c5615165c8d524b5bc6e41382c4003b992922
reno1314 <80469553+reno1314@users.noreply.github.com>
2025-06-08 20:26:22 +0800

    Update XianSu_1.52_S5_auto.sh

--- XianSu_1.52_S5_auto.sh
+++ XianSu_1.52_S5_auto.sh
@@ -20,7 +20,7 @@ sed -i '2c for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;do
 cd /root
 rm -f /var/spool/cron/root
 touch /var/spool/cron/root
-echo "@reboot sleep 35 && bash /root/sk5_auto_XS1.52.sh" >> /var/spool/cron/root
+echo "@reboot sleep 20 && bash /root/sk5_auto_XS1.52.sh" >> /var/spool/cron/root
 
 for((i=2;i<=30;i++));do /sbin/ip address add 10.0.0.$i/24 dev eth0;done
 sudo yum -y install wget && wget --no-check-certificate https://raw.githubusercontent.com/reno1314/danted/master/install_R.sh -O install.sh && bash install.sh  --port=12479 --user=123 --passwd=123 && /etc/init.d/sockd start
