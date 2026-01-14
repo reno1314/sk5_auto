cd89d7dd11444a381b4a0f36a93cbb135c6f6c8d
reno1314 <80469553+reno1314@users.noreply.github.com>
2026-01-14 16:00:28 +0800

    Update speed_new_15.sh

--- speed_new_15.sh
+++ speed_new_15.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f /root/control.sh
-curl -kLs https://raw.githubusercontent.com/reno1314/sk5_auto/master/speed_limit_each_new_15.sh -o control.sh && chmod +x control.sh && ./control.sh set
+curl -kLs https://raw.githubusercontent.com/reno1314/sk5_auto/master/speed_limit_each_new_10.sh -o control.sh && chmod +x control.sh && ./control.sh set
 
 cd /root
 [ ! -f /var/spool/cron/root ] && touch /var/spool/cron/root
